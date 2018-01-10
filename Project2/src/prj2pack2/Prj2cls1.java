93d2d2c28e323282d237c9abfbace2959a0fe955
RajaDBA <RajaDBA@192.168.0.19>
2018-01-09 22:18:27 -0500

    updated 1/9/2018 at 10:00 p.m

--- Project2/src/prj2pack2/Prj2cls1.java
+++ Project2/src/prj2pack2/Prj2cls1.java
@@ -3,7 +3,7 @@ package prj2pack2;
 public class Prj2cls1 {
 
 	public static void main(String[] args) {
-		//welcome
+		//welcome to Java Training
 		System.out.println("This is project2 class1");
 
 	}
